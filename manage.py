f58ee92e489fec372a6837ff976715d041b6b84c
root <root@localhost.localdomain>
2019-11-20 01:22:07 +0800

    初始化的manage，准备开始拆分项目目录

--- manage.py
+++ manage.py
@@ -1,7 +1,40 @@
 
 from flask import Flask
+from flask_sqlalchemy import SQLALCHEMY
+from flask_session import Session
+from flask_wtf import CSRFProtect
+
+import redis
 
 app = Flask(__name__)
+
+class Config:
+    DEBUG = True
+    SECRET_KRY = "nacokmvADOJ*^1wcmdcqps"
+
+    # 数据库
+    SQLALCHEMY_DATABASE_URI = ""
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    # redis配置
+    REDIS_HOST = ""
+    REDIS_PORT = ""
+
+    SESSION_TYPE = ""
+    SESSION_REDIS = redis.StrictRedis(host="", port=6379)
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = 86400
+
+app.congig.from_object(Config)
+
+db = SQLALCHEMY(app)
+
+redis_store = redis.StrictRedis()
+
+#
+Session(app)
+
+CSRFProtect(app)
 
 @app.route("/index")
 def index():
